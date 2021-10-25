23a14b8bd269822ba67d74e05ac6c3fb90bcf4d9
uijong0729 <krocha0729@gmail.com>
2021-10-25 23:08:46 +0900

    c string

---
+++ CStyleString.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+// 현업의 C++애플리케이션에서는 여전히 성능상의 이유로 많은 C함수들을 사용
+int main() {
+	char str[10] = "string";
+
+	// 문자열의 길이 구하기
+	// size_t strlen(const char* str);
+	size_t length = strlen(str);
+	cout << "길이 : " << length;
+
+
+
+	return 0;
+}

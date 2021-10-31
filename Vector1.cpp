83fa90f94e32762daaa4a20a5da6767a7b9ffa66
uijong0729 <krocha0729@gmail.com>
2021-10-31 22:35:24 +0900

    class exam

---
+++ Vector1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Vector1.h"
+
+using namespace std;
+
+// 积己磊 备泅何
+Vector1::Vector1() : mX(0), mY(0) 
+{
+	cout << mX << mY << endl;
+}
+
+// 积己磊 备泅何
+Vector1::Vector1(int x, int y)
+	: mX(x)
+	, mY(y)
+{
+	cout << mX << mY;
+}
---
+++ Vector1.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Vector1
+{
+public:
+	// 생성자
+	Vector1();
+	Vector1(int x, int y);
+
+private:
+	int mX;
+	int mY;
+}; // 클래스 선언 끝에는 ;를 붙임
